1895c7901aae84dd3d49b0757e91d83b4ee9d2f3
MadeInChina <slamhan1987@hotmail.com>
2012-08-15 14:40:06 +0800

    added home button test

--- src/main/java/com/hrw/android/tdd/activity/HomeActivity.java
+++ src/main/java/com/hrw/android/tdd/activity/HomeActivity.java
@@ -2,7 +2,20 @@
 package com.hrw.android.tdd.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.hrw.android.R;
 
 public class HomeActivity extends Activity {
+
+    public ImageView mHomeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.home_layout);
+        mHomeButton = (ImageView) findViewById(R.id.home_button);
+        super.onCreate(savedInstanceState);
+    }
 
 }
--- src/test/java/com/hrw/android/tdd/activity/HomeActivityTest.java
+++ src/test/java/com/hrw/android/tdd/activity/HomeActivityTest.java
@@ -1,8 +1,11 @@
 
 package com.hrw.android.tdd.activity;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 
 import com.hrw.android.R;
@@ -10,10 +13,24 @@ import com.xtremelabs.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class HomeActivityTest {
+    HomeActivity mHomeActivity;
+
+    @Before
+    public void setUp() {
+        mHomeActivity = new HomeActivity();
+        mHomeActivity.onCreate(null);
+    }
 
     @Test
     public void should_return_app_name() throws Exception {
         assertEquals("Android Tdd Sample",
-                new HomeActivity().getResources().getString(R.string.app_name));
+                mHomeActivity.getResources().getString(R.string.app_name));
+    }
+
+    @Test
+    public void should_return_home_button() throws Exception {
+        assertNotNull(mHomeActivity.mHomeButton);
+        assertEquals(R.id.home_button,
+                 mHomeActivity.mHomeButton.getId());
     }
 }
